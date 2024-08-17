cbf5c5c9f72548864d44d62d57bfb069c849633d
gbrs <gbrs@mail.ru>
2024-08-17 22:20:34 +0300

    solve task 1 from lesson 9

--- 09.py
+++ 09.py
@@ -3,7 +3,6 @@
 """
 
 
-
 """
 ------------------
 
@@ -19,6 +18,15 @@
 
 ------------------
 
+1
+lst = []
 
+with open('a_few_lines_of_text.txt', encoding='utf-8') as f:
+    for i, line in enumerate(f):
+        lst.append(f"{i + 1}: " + line)
+
+with open('numbered_lines_of_text.txt', 'wt', encoding='utf-8') as f:
+    for i in lst:
+        f.write(i)
 
 """
